029db7f16bb98aee793bfd2a077fd48dcb6896e7
Vincent Qi <vincent.qi.2006@gmail.com>
2025-07-26 00:43:15 -0700

    chore: remove console logs

--- packages/engine/services/dockerEngine.ts
+++ packages/engine/services/dockerEngine.ts
@@ -130,15 +130,12 @@ class DockerEngineService {
         throw new Error(`Stream request failed: ${stream.status} ${stream.statusText}`);
       }
 
-      console.log("Stream OK");
-
       const reader = stream.body?.getReader();
       if (!reader) throw new Error("Reader not found");
 
       try {
         while (true) {
           const { done, value } = await reader.read();
-          console.log(`SHOULD SEND: `, value);
           if (done) break;
           socket.emit('stream-data', value);
         }
